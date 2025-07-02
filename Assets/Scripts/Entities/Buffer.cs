636b42c2c557b20f525e844f7cdbf262148dd109
temmmi0 <temmmi.bits.stdcpp.h@gmail.com>
2025-07-02 17:02:28 +0300

    added buffer

---
+++ Assets/Scripts/Entities/Buffer.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Buffer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private string cur;
+    
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+    }
+}
